2da6a4b9105f5e95116e8bf02aeac4c033d090ed
Paulo Luís <paulo_nunis2@hotmail.com>
2017-07-12 19:40:36 -0300

    Update trabalhon2.py

--- trabalhon2.py
+++ trabalhon2.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
 # -- coding: utf-8 --
+
+from urllib.request import Request, urlopen
+import json
+
+MATRICULA = ''
+TOKEN = ''
+AUTHORIZATION = ''
+
+req = Request('https://suap.ifrn.edu.br/api/v2/edu/alunos/{}/'.format(MATRICULA))
+req.add_header('Accept', 'application/json')
+req.add_header('X-CSRFToken', TOKEN)
+req.add_header('Authorization', AUTHORIZATION)
+
+dados_byte = urlopen(req).read()
+dados_txt = dados_byte.decode('utf-8')
+print(dados_txt)                        
