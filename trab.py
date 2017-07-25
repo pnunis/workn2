08f4a802d553debf57b43ae61c514564fe557be8
Paulo Luís <paulo_nunis2@hotmail.com>
2017-07-24 21:21:34 -0300

    Update trab.py
    retirando o comentario da linha 11

--- trab.py
+++ trab.py
@@ -8,7 +8,7 @@ MATRICULA = '20141014050296'
 TOKEN = '0GrRTeQzIh03nn4I6hGqdgI45ksp7hOoft4P0BOGa9jKzqeBQ7N1O4zMZmXHt1gE'
 AUTHORIZATION = 'Basic MjAxNDIwMTQwNTAzNjc6ODg0MTQ4OTNpZiM='
 
-#req = Request('https://suap.ifrn.edu.br/api/v2/edu/alunos/{}/'.format(MATRICULA))
+req = Request('https://suap.ifrn.edu.br/api/v2/edu/alunos/{}/'.format(MATRICULA))
 req.add_header('Accept', 'application/json')
 req.add_header('X-CSRFToken', TOKEN)
 req.add_header('Authorization', AUTHORIZATION)
